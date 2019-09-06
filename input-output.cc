d132ab01d90cfe3639bc4da04de32b10531c3080
Nasseef Abukamail <abukamai@ohio.edu>
2019-09-06 14:45:36 -0400

    cout examples

---
+++ input-output.cc
@@ -0,0 +1,25 @@
+/*
+ *        File: input-output.cc
+ *      Author: Nasseef Abukamail
+ *        Date: September 06, 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+//function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello\" \\ \tWorld\n";
+    int age = 40;
+    cout << "My ages is " << age << '.' << endl;
+    cout << (9 + 5) << endl;
+    string name = "Bob";
+    cout << "My name is " << name << endl;
+    cout << "My name is " + name << endl;
+    return 0;
+}// main
