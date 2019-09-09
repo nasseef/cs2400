2e8e2d128eed78a65731dfa944438552a5564765
Nasseef Abukamail <abukamai@ohio.edu>
2019-09-09 12:02:20 -0400

    Added an output formatting example

--- input-output.cc
+++ input-output.cc
@@ -20,5 +20,8 @@ int main(int argc, char const *argv[]) {
     cout << (9 + 5) << endl;
     string name = "Bob";
     cout << "My name is " << name << endl;
+    double salary = 2000000000.24567;
+    cout << fixed << setprecision(2) << showpoint;
+    cout << "Salary is $" << salary << endl;
     return 0;
 }// main
