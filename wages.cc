920eea7654a0191c7b01547baf91a34f8482cd0e
Nasseef Abukamail <abukamai@ohio.edu>
2019-09-13 16:29:31 -0400

    Added overtime pay and more formatting

--- wages.cc
+++ wages.cc
@@ -23,11 +23,21 @@ int main(int argc, char const *argv[]) {
     cout << "Enter the number of hours: ";
     cin >> hours;
 
-    ///#3
-    wages = hours * rate;
+    ///#3 Calculate wages (overtime?)
+    if (hours > 40)
+    {
+        double overtime = (hours - 40) * rate * 1.5;///overtime
+        double regularPay = 40 * rate; ///regular
+        wages = overtime + regularPay;
+    } else {
+        wages = hours * rate;
+    }
+    
 
     ///#
     cout << fixed << setprecision(2) << showpoint;
-    cout << "Weekly pay: $" << wages << endl;
+    cout << setw(25) << "Your hourly rate: $" << rate << endl;
+    cout << setw(24) << "Hours worked: " << hours << endl;
+    cout << setw(25) << "Weekly pay: $" << wages << endl;
     return 0;
 }// main
