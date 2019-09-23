658aef3ed5a788a48f5469046ce9226f89613353
Nasseef Abukamail <abukamai@ohio.edu>
2019-09-23 19:43:05 -0400

    switch example

--- switch.cc
+++ switch.cc
@@ -19,7 +19,9 @@ int main() {
         case  1:
             cout << "You entered 1" << endl;
             //break;
-        case 2: case 3:
+        case 2: 
+        
+        case 3:
             cout << "You entered 2 or 3" << endl;
             break;
         case 4:
