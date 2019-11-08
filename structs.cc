6e117551465b7d474a8f1faeb71ec9a66261bf39
Nasseef Abukamail <abukamai@ohio.edu>
2019-11-08 12:49:56 -0500

    More examples of structures

--- structs.cc
+++ structs.cc
@@ -12,7 +12,7 @@
 using namespace std;
 
 // declare a structure
-struct Student {
+class Student {
     int id;
     string name;
     double score;
@@ -20,22 +20,46 @@ struct Student {
 
 /// function prototypes
 
-void displayStudent(Student s);
+void displayStudent(const Student &s);
+Student inputStudent();
+
 int main(int argc, char const *argv[]) {
     Student s;
     s.id = 123;
     s.name = "Bob";
     s.score = 65;
+    s.id = -12;
 
     displayStudent(s);
-    Student s2 = {234, "Jim", 75};
+    Student s2 = inputStudent();
     displayStudent(s2);
+    vector <Student> allStudents;
+    allStudents.push_back(s2);
+    allStudents.push_back(s);
+    cout << "*************" << endl;
+    // for (size_t i = 0; i < allStudents.size(); i++)
+    // {
+    //     displayStudent(allStudents.at(i));
+    // }
+    for (Student s : allStudents)
+    {
+        displayStudent(s);
+    }
+    
+    
     return 0;
 }  /// main
 
-void displayStudent(Student s) {
+void displayStudent(const Student &s) {
     cout << "ID: " << s.id << endl;
     cout << "Name: " << s.name << endl;
     cout << "Score: " << s.score << endl;
 }
 
+Student inputStudent(){
+    Student s;
+    cout << "Enter student info" << endl;
+    cin >> s.id >> s.name >> s.score;
+    return s;
+
+}
