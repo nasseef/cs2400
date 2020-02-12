c052cca935dc17f152e09f058f13da3eb0c891ec
Nasseef Abukamail <abukamai@ohio.edu>
2020-02-11 21:17:46 -0500

    wages run continuously

--- wages.cc
+++ wages.cc
@@ -5,47 +5,48 @@
  *  @brief: Add Description
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 using namespace std;
-///constants are declared globally
+/// constants are declared globally
 
-const double PI = 3.14159;
-///function prototypes
+/// function prototypes
 
 int main(int argc, char const *argv[]) {
-    ///1
+    /// 1
     double hours;
     double rate;
     double wages;
-    
-    ///read/input the number of hours
-    cout << "Enter number of hours: ";
-    cin >> hours;
-    if (hours < 0)
-    {
-        cout << "Error: hours must be >= 0" << endl;
-        exit(0);
-    }
-    
-    /// read the rate
-    cout << "Enter the rate: ";
-    cin >> rate;
-    if (rate < 0)
-    {
-        cout << "Error: rate must be > 0" << endl;
-        //exit(0);
-    }
-    
-    ///cin >> hours >> rate;
-    ///2
-    wages = hours * rate;
+    char answer = 'y';
+    while (answer == 'y') {
+        /// read/input the number of hours
+        cout << "Enter number of hours: ";
+        cin >> hours;
+        if (hours < 0) {
+            cout << "Error: hours must be >= 0" << endl;
+            continue;
+        }
 
-    ///3
-    cout << fixed << setprecision(2) << showpoint; //formatting flags
-    cout << "Hours: " << hours << endl;
-    cout << "Hourly rate: $" << rate << endl;
-    cout << "Wages: $" << wages << endl;
+        /// read the rate
+        cout << "Enter the rate: ";
+        cin >> rate;
+        if (rate < 0) {
+            cout << "Error: rate must be > 0" << endl;
+            continue;
+        }
+
+        /// cin >> hours >> rate;
+        /// 2
+        wages = hours * rate;
+
+        /// 3
+        cout << fixed << setprecision(2) << showpoint;  // formatting flags
+        cout << "Hours: " << hours << endl;
+        cout << "Hourly rate: $" << rate << endl;
+        cout << "Wages: $" << wages << endl;
+        cout << endl << "More employees(y/n)? ";
+        cin >> answer;
+    }
     return 0;
-}/// main
+}  /// main
