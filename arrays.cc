b2c49308e6e805ad49a1d10c835df8010efe4801
Nasseef Abukamail <abukamai@ohio.edu>
2020-03-25 11:38:16 -0400

    More array examples

--- arrays.cc
+++ arrays.cc
@@ -11,10 +11,12 @@
 using namespace std;
 
 ///function prototypes
+void printArray(const int numbers[], int numStudents);
+void inputArray(int numbers[], int &numStudents);
 
 int main(int argc, char const *argv[]) {
     //declaration
-    int numbers[10] = {15, 20, 5, 35, 30, 10, 25};
+    int numbers[10] = {15, 20, 5, 35, 30, 10, 25}; //static declaration
     cout << "First element: " << numbers[2] << endl;
     numbers[1] = 12;
     cout << "The number at position 1 is " << numbers[1] << endl;
@@ -22,19 +24,36 @@ int main(int argc, char const *argv[]) {
     int numStudents = 7;
     numbers[numStudents] = 99;
     numStudents++;
-    for(int i = 0; i < numStudents; i++){
-        cout << numbers[i] << " ";
-    }
-    cout << endl;
+    printArray(numbers, numStudents);
 
     int temp = numbers[0];
     numbers[0] = numbers[numStudents - 1];
     numbers[numStudents - 1] = temp;
+    
+    inputArray(numbers, numStudents);
+    cout << "Number of students: " << numStudents << endl;
+    printArray(numbers, numStudents);
 
+    return 0;
+}/// main
+
+void printArray(const int numbers[], int numStudents){
     for(int i = 0; i < numStudents; i++){
         cout << numbers[i] << " ";
     }
     cout << endl;
+}
 
-    return 0;
-}/// main
+void inputArray(int numbers[], int &numStudents){
+    int num;
+    int i = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+    while(num >= 0){
+        numbers[i] = num;
+        i++;
+        cout << "Enter a number: ";
+        cin >> num;
+    }
+    numStudents = i;
+} 
