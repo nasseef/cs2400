ee919fcfdf883cc5ea8e11dfbf84505a41e6fdaa
Nasseef Abukamail <abukamai@ohio.edu>
2020-08-28 09:40:34 -0400

    first program

---
+++ first.cc
@@ -0,0 +1,18 @@
+/**
+ *   @file: first.cc
+ * @author: Nasseef Abukamail
+ *   @date: August 28, 2020
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+
+using namespace std;
+
+///function prototypes
+
+int main() { //main function is the entry point to your program
+
+    cout << "Hello World!" << endl;
+    return 0;
+} /// main
