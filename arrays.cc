3caddbdd36113bbe73a287d36640a763dcfddf80
Nasseef Abukamail <abukamai@ohio.edu>
2020-10-19 11:21:47 -0400

    Arrays and functions

--- arrays.cc
+++ arrays.cc
@@ -12,41 +12,54 @@
 using namespace std;
 const int SIZE = 10;
 /// function prototypes
+void printArray(const int numbers[], int count);
+void inputArray(int numbers[], int &count);
+int getTotal(const int numbers[], int count);
 
 int main(int argc, char const *argv[]) {
 
     int numbers[SIZE]; //garbage in positions 5-9, 10 elements
-    // int numbers2[] = {12, 4, 9, 8, 5}; //number of elements is 5
 
-    int count = 0;
+    int count;
 
-    // 12, 5, 9, 8 , 7, -9
+    inputArray(numbers, count);
 
-    int num;
-    cout << "Enter numbers terminated by a negative value" << endl;
-    int i = 0;
-    cin >> num;
-    
-    while (num >= 0)
-    {
-        count++;
-        numbers[i] = num;
-        i++;
-        cin >> num;
-    }
-    
     cout << "Count is " << count << endl;
 
+    printArray(numbers, count);
+    
+    int total = getTotal(numbers, count);
+    cout << "Total: " << total << endl;
+    return 0;
+}  /// main
+
+void printArray(const int numbers[], int count){
     for (size_t i = 0; i < count; i++) //size_t === unsigned int
     {
         cout << numbers[i] << endl;
     }
     
-    // numbers[0] = 23;
-    // numbers[1] = 5;
-    // numbers[2] = 12;
+}
 
-    // cout << (numbers[0] + 8)<< endl;
-    // cout << sqrt(numbers[2]) << endl;
-    return 0;
-}  /// main
+void inputArray(int numbers[], int &count){
+    count = 0;
+
+    cout << "Enter numbers terminated by a negative value" << endl;
+    int num;
+    cin >> num;
+    while (num >= 0)
+    {
+        numbers[count] = num;
+        count++;
+        cin >> num;
+    }
+}
+
+int getTotal(const int numbers[], int count){
+    int sum = 0;
+    for (size_t i = 0; i < count; i++)
+    {
+        sum += numbers[i];
+    }
+    return sum;
+}
