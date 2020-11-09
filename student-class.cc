0221254040a92dd90ac0155b7f061c8b8f6fc8ec
Nasseef Abukamail <abukamai@ohio.edu>
2020-11-09 11:43:59 -0500

    Added constructors and other helper functions

--- student-class.cc
+++ student-class.cc
@@ -12,6 +12,13 @@ using namespace std;
 
 class Student{
 public:
+    //constructors
+    Student(); //default constructor
+    Student(string newName, int newId, double newScore);
+    Student(string newName, int newId);
+    
+
+
     //setters/mutators
     void setId(int newId); 
     void setName(string newName);
@@ -22,6 +29,10 @@ public:
     string getName();
     double getScore();
 
+    //helper functions
+    void output();
+    void input();
+    char getGrade();
 private:
     string name;
     int id;
@@ -32,20 +43,24 @@ private:
 
 int main(int argc, char const *argv[]) {
 
-    Student s;
-    s.setId(0);  //replace s.id = 123;
-
-    int myId = s.getId();
-    cout << "My Id " << myId << endl;
-    s.setId(234);
-    cout << "My Id: " << s.getId() << endl;
-
-    s.setName("");
-    cout << "Name: " << s.getName() << endl;
+    Student s; //call the default constructor
+    //use s
+    s.output();
+    
+    Student s2("Mary", 345, -99);
+    s2.setId(-99);
+    s2.output();
+    
     return 0;
 } /// main
 
-void Student::setId(int newId){ //member function of the class Student
+Student::Student(){
+    id = 0;
+    name = "N/A";
+    score = -1;
+}
+
+Student::Student(string newName, int newId, double newScore){
     if (newId >= 0) //0 is my default
     {
         id = newId;
@@ -53,6 +68,30 @@ void Student::setId(int newId){ //member function of the class Student
     else {
         id = 0; //default based on business rules
     }
+    if (newName != "") //business rule
+    {
+        name = newName; 
+    }
+    else {
+        name = "N/A"; //default
+    }
+    if (newScore >= 0)
+    {
+        score = newScore;
+    }
+    else {
+        score = -1;
+    }
+    
+
+}
+
+void Student::setId(int newId){ //member function of the class Student
+    if (newId >= 0) //0 is my default
+    {
+        id = newId;
+    }
+
 }
 
 int Student::getId(){
@@ -64,11 +103,14 @@ void Student::setName(string newName){
     {
         name = newName; 
     }
-    else {
-        name = "N/A"; //default
-    }
 }
 
 string Student::getName(){
     return name;
+}
+
+void Student::output(){
+    cout << " Name: " << name << endl;
+    cout << "   ID: " << id << endl;
+    cout << "Score: " << score << endl;
 }
