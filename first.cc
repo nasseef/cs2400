fc5f6e222ab416d4607389f290a24098d86f420b
Nasseef Abukamail <abukamai@ohio.edu>
2021-08-30 11:46:40 -0400

    first program

---
+++ first.cc
@@ -0,0 +1,28 @@
+/**
+ *   @file: first.cc
+ * @author: Nasseef Abukamail
+ *   @date: August 30, 2021
+ *  @brief: Add Description
+ */
+
+//compiler directives
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+/// Constants and function prototypes
+
+int main(int argc, char const *argv[])  // function heading
+{
+    int number = 5;
+    //use one of these styles for naming identifiers such as a variable name
+    int mySalary, my_salary;
+    number = 12;
+    number = number + 5;
+
+    cout << "The value of number is " << number << endl;
+
+    return 0;  // EXIT_SUCCESS
+}  /// main
