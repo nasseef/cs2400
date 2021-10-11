82c6ef5748ea7ec0ef1574a6b15e2311cbb995da
Nasseef Abukamail <abukamai@ohio.edu>
2021-10-11 11:52:53 -0400

    stream example using get function

---
+++ files-get.cc
@@ -0,0 +1,51 @@
+/**
+ *   @file: files.cc
+ * @author: Nasseef Abukamail
+ *   @date: October 11, 2021
+ *  @brief: Add Description
+ */
+
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+/// Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+    ifstream inStream;
+    string filename;
+    cout << "Enter the file name: ";
+    cin >> filename;
+    inStream.open(filename);
+    if (inStream.fail())
+    {
+        cout << "Error: file not found" << endl;
+        exit(0);
+    }
+    
+    ofstream outStream;
+    outStream.open("output.txt");
+    if (outStream.fail())
+    {
+        cout << "Error: failed to creat file" << endl;
+        exit(0);
+    }
+    
+    //opening of the stream is successful
+    string word;
+    char ch;
+    inStream.get(ch);
+    while (!inStream.eof())
+    {
+       outStream << ch;
+       inStream.get(ch);
+    }
+    
+    cout << endl;
+    inStream.close();
+    outStream.close();
+    return 0;
+}  /// main
