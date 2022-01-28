cefed9acda17c4f4b587534bee0c38ec8afb4ce7
Nasseef Abukamail <abukamai@ohio.edu>
2022-01-28 17:56:44 -0500

    Added input validations

--- wages.cc
+++ wages.cc
@@ -11,6 +11,7 @@
 using namespace std;
 
 ///Constants and function prototypes
+const double MAX_HOURS = 70;
 
 int main(int argc, char const *argv[]) {
 
@@ -19,11 +20,24 @@ int main(int argc, char const *argv[]) {
     //1. input the hours
     cout << "Enter the number of hours: ";
     cin >> hours;
+    if (hours < 0 || hours > MAX_HOURS) //invalid?
+    {
+        cout << "Error: hours must be between 0 and 70" << endl;
+        //return 0; //terminates the function
+        exit(0);  //terminates the program
+    }
+    
     //2. input the rate
     cout << "Enter the hourly rate: ";
     cin >> rate;
-
+    if (rate < 0 || rate > 20)
+    {
+       cout << "Error: rate must be between 0 and 20" << endl;
+       exit(0);
+    }
+    
     //3. Calculations
+    //calculate overtime pay
     wages = hours * rate;
 
     //4. Output
