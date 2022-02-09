681292a8fc8e31f68ec0948dc27285cb44ef2436
Nasseef Abukamail <abukamai@ohio.edu>
2022-02-09 14:42:06 -0500

    Example for looping unitl input fails

---
+++ input-failure.cc
@@ -0,0 +1,38 @@
+/**
+ *   @file: input-failure.cc
+ * @author: Nasseef Abukamail
+ *   @date: February 9, 2022
+ *  @brief: Add Description
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+/// Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+    int number, total = 0;
+    cout << "Enter a number, Q to quit: ";
+    cin >> number;
+    while (!cin.fail()) {
+        total += number;
+        cout << "Enter a number, Q to quit: ";
+        cin >> number;
+    }
+    cout << "Total: " << total << endl;
+    if (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(100, '\n');
+    }
+
+
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Your name is " << name << endl;
+
+    return 0;
+}  /// main
