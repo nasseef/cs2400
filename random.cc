cb0d28cc554791e08ab91d52427ffa2d5250c52a
Nasseef Abukamail <abukamai@ohio.edu>
2022-02-11 14:54:20 -0500

    Random number generator example

---
+++ random.cc
@@ -0,0 +1,27 @@
+/**
+ *   @file: random.cc
+ * @author: Nasseef Abukamail
+ *   @date: September 21, 2020
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///function prototypes
+
+int main(int argc, char const *argv[]) {
+    srand(time(nullptr)); //seeding the random number generator (only once)
+    
+    for (size_t i = 0; i < 10; i++)
+    {
+        int randomNumber = rand();
+        randomNumber = randomNumber % 6 + 1;
+        cout << "Random number is " << randomNumber << endl;
+    }
+    
+    /*add code*/
+    return 0;
+} /// main
