9fb630d2cb320ec3ee905130b749626603e75ad1
Nasseef Abukamail <abukamai@ohio.edu>
2022-03-14 16:04:32 -0400

    Array example with functions

--- arrays.cc
+++ arrays.cc
@@ -17,7 +17,7 @@ void printArray(int numbers[], int count);
 int getTotal(int numbers[], int count);
 int main(int argc, char const *argv[]) {
 
-    int numbers[15000] = {12, 9, 3, 16, 10, 1, 4, 6, 8, 15};
+    int numbers[15] = {12, 9, 3, 16, 10, 1, 4, 6, 8, 15};
     int count = 10;
     printArray(numbers, count);
     swap(numbers[0], numbers[5]);
