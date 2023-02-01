d48b8ff68bb2c8616675af45f9a139e5a994f01b
Nasseef Abukamail <abukamai@ohio.edu>
2023-02-01 17:41:13 -0500

    Added input validation

--- wages.cc
+++ wages.cc
@@ -20,15 +20,31 @@ int main(int argc, char const *argv[]) {
     //2. input rate and hours
     cout << "Enter the hourly rate: ";
     cin >> rate;
-
+    if (rate < 15 || rate > 35)
+    {
+        cout << "Error: rate invalid" << endl;
+        exit(0);
+    }
+    
     cout << "Enter the number of hours: ";
     cin >> hours;
-
+    if (hours >= 0 && hours <= 40)
+    {
+    
+    
     //3. Calculating the wages
     wages = hours * rate;
 
-    cout << "The hourly rate is " << rate << endl;
+    cout << fixed << setprecision(2);  //scientific
+    double x = 12345678910000.345;
+    cout << "x = " << x << endl;
+    cout <<"The hourly rate is $" << rate << endl;
+
     cout << "The number of hours is " << hours << endl;
-    cout << "The wages: " << wages << endl;
+    cout << "The wages: $" << wages << endl;
+    }
+    else {
+        cout << "Error: hours is invalid" << endl;
+    }
     return 0;
 } /// main
