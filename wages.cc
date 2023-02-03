c4246a7ced0149b45ed0e120d0136366508dd9a8
Nasseef Abukamail <abukamai@ohio.edu>
2023-02-03 10:05:48 -0500

    Added some formatting

--- wages.cc
+++ wages.cc
@@ -5,45 +5,39 @@
  *  @brief: Add Description
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
-///Constants and function prototypes
+/// Constants and function prototypes
 
 int main(int argc, char const *argv[]) {
-
-    //1. declare variable
+    // 1. declare variable
     double rate, hours, wages;
 
-    //2. input rate and hours
+    // 2. input rate and hours
     cout << "Enter the hourly rate: ";
     cin >> rate;
-    if (rate < 15 || rate > 35)
-    {
+    if (rate < 15 || rate > 35) {
         cout << "Error: rate invalid" << endl;
         exit(0);
     }
-    
+
     cout << "Enter the number of hours: ";
     cin >> hours;
-    if (hours >= 0 && hours <= 40)
-    {
-    
-    
-    //3. Calculating the wages
-    wages = hours * rate;
+    if (hours >= 0 && hours <= 40) {
+        // 3. Calculating the wages
+        wages = hours * rate;
 
-    cout << fixed << setprecision(2);  //scientific
-   
-    cout <<"The hourly rate is $" << rate << endl;
+        cout << fixed << setprecision(2);  // scientific
 
-    cout << "The number of hours is " << hours << endl;
-    cout << "The wages: $" << wages << endl;
-    }
-    else {
+        cout << "The hourly rate is $" << rate << endl;
+
+        cout << "The number of hours is " << hours << endl;
+        cout << "The wages: $" << wages << endl;
+    } else {
         cout << "Error: hours is invalid" << endl;
     }
     return 0;
-} /// main
+}  /// main
