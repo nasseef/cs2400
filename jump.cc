80612bcea7422171f7e7deb103dae3d3888f28f1
Nasseef Abukamail <abukamai@ohio.edu>
2023-02-13 11:43:34 -0500

    Counter example with while loop

---
+++ jump.cc
@@ -0,0 +1,26 @@
+/**
+ *   @file: jump.cc
+ * @author: Nasseef Abukamail
+ *   @date: February 13, 2023
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    int numJumps = 3;
+    int count = 0;
+    while (count < numJumps)
+    {
+        cout << "Jump" << endl;
+        count++;
+    }
+    
+    return 0;
+} /// main
