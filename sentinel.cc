5251fdf7935774936385047c5ec8782b66d5f3a6
Nasseef Abukamail <abukamai@ohio.edu>
2023-02-13 11:44:10 -0500

    Sentinel with counting and calculating average

--- sentinel.cc
+++ sentinel.cc
@@ -5,27 +5,37 @@
  *  @brief: Add Description
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
-///Constants and function prototypes
+/// Constants and function prototypes
 
 int main(int argc, char const *argv[]) {
-
     double total = 0;
     double num = 0;
+    int count = 0;
 
     cout << "Enter a number: ";
-    cin >> num; //I
-    while (num >= 0) //C
+    cin >> num;       // I
+    while (num >= 0)  // C
     {
-        total += num; // total = total + num
+        count++;
+        total += num;  // total = total + num
         cout << "Enter a number: ";
-        cin >> num; //U
+        cin >> num;  // U
     }
     cout << "The total is " << total << endl;
-    
+    cout << "The count is " << count << endl;
+
+    double average;
+    if (count == 0) {
+        cout << "count is zero, no average" << endl;
+    } else {
+        average = total / count;
+        cout << "The average is " << average << endl;
+    }
+
     return 0;
-} /// main
+}  /// main
