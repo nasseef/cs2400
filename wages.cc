987a464f4ae776d7c3f3ab90a10d5d583afbc2f7
Nasseef Abukamail <abukamai@ohio.edu>
2023-02-16 09:34:09 -0500

    Added a repetition y/n question

--- wages.cc
+++ wages.cc
@@ -15,29 +15,33 @@ using namespace std;
 int main(int argc, char const *argv[]) {
     // 1. declare variable
     double rate, hours, wages;
+    char answer;
+    do {
+        // 2. input rate and hours
+        cout << "Enter the hourly rate: ";
+        cin >> rate;
+        if (rate < 15 || rate > 35) {
+            cout << "Error: rate invalid" << endl;
+            exit(0);
+        }
 
-    // 2. input rate and hours
-    cout << "Enter the hourly rate: ";
-    cin >> rate;
-    if (rate < 15 || rate > 35) {
-        cout << "Error: rate invalid" << endl;
-        exit(0);
-    }
+        cout << "Enter the number of hours: ";
+        cin >> hours;
+        if (hours >= 0 && hours <= 40) {
+            // 3. Calculating the wages
+            wages = hours * rate;
 
-    cout << "Enter the number of hours: ";
-    cin >> hours;
-    if (hours >= 0 && hours <= 40) {
-        // 3. Calculating the wages
-        wages = hours * rate;
+            cout << fixed << setprecision(2);  // scientific
 
-        cout << fixed << setprecision(2);  // scientific
+            cout << "The hourly rate is $" << rate << endl;
 
-        cout << "The hourly rate is $" << rate << endl;
-
-        cout << "The number of hours is " << hours << endl;
-        cout << "The wages: $" << wages << endl;
-    } else {
-        cout << "Error: hours is invalid" << endl;
-    }
+            cout << "The number of hours is " << hours << endl;
+            cout << "The wages: $" << wages << endl;
+        } else {
+            cout << "Error: hours is invalid" << endl;
+        }
+        cout << "Continue (y/n)? ";
+        cin >> answer;
+    } while (answer == 'y');
     return 0;
 }  /// main
