86ebab7052b5c692e82efaa9c5a07f2a3029d800
Nasseef Abukamail <abukamai@ohio.edu>
2023-03-08 11:48:55 -0500

    simple array example

---
+++ array.cc
@@ -0,0 +1,27 @@
+/**
+ *   @file: array.cc
+ * @author: Nasseef Abukamail
+ *   @date: March 08, 2023
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    int numbers[10] = {5, 9, 2, 6, 7};
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout << numbers[i] << endl;
+    }
+    
+   
+
+    
+    return 0;
+} /// main
