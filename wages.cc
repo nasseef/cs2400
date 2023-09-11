199698c545fc2ce0b0f3c07995d8096455624fe1
Nasseef Abukamail <abukamai@ohio.edu>
2023-09-11 08:52:30 -0400

    now validates the input

--- wages.cc
+++ wages.cc
@@ -18,14 +18,31 @@ int main(int argc, char const *argv[]) {
     double rate, hours, wages;
 
     // input
-    rate = 1012;
-    hours = 30;
+    // rate = 1012;
+    // hours = 30;
 
+    cout << "Enter the rate: ";
+    cin >> rate;
+    if (rate <= 0) //invalid
+    {
+        cout << "Error: rate must be greater than 0" << endl;
+        exit(0);
+    }
+    
+    cout << "Enter the hours: ";
+    cin >> hours;
+    if (hours < 0)
+    {
+        cout << "Error: hours must be 0 or more" << endl;
+        exit(0);
+    }
+    
     /// 2 calculations
     wages = rate * hours;
 
     //3. output
-    cout << "Wages: " << wages << endl;
+    cout << fixed << setprecision(2);
+    cout << "Wages: $" << wages << endl;
 
     return 0;
 } /// main
