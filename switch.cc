1b2477818c918607e5b8ca9c9ba03a381b9580ca
Nasseef Abukamail <abukamai@ohio.edu>
2023-09-18 11:50:40 -0400

    switch example

---
+++ switch.cc
@@ -0,0 +1,34 @@
+/**
+ *   @file: switch.cc
+ * @author: Nasseef Abukamail
+ *   @date: September 18, 2023
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+    
+    int choice;
+    ///display a menu of choices
+    cout << "Enter a choice: ";
+    cin >> choice;
+    switch(choice){
+        case 1: cout << "choice 1" << endl;
+                break;
+        case 2: cout << "choice 2" << endl;
+                break;
+        case 3: 
+        case 4: cout << "choice 3 or 4" << endl;
+                break;
+        default:
+            cout << "you entered something else" << endl;
+    }
+    cout << "Done" << endl;
+    return 0;
+} /// main
