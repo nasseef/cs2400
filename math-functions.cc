cf342f5fdbfdbfb956a615f507b3facbdaa24c78
Nasseef Abukamail <abukamai@ohio.edu>
2023-09-20 13:28:21 -0400

    examples of math functions

---
+++ math-functions.cc
@@ -0,0 +1,20 @@
+/**
+ *   @file: math-functions.cc
+ * @author: Nasseef Abukamail
+ *   @date: September 20, 2023
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    double value = sqrt(7) * pow(2, 3) + 5;
+    cout << "value is " << value << endl;
+    return 0;
+} /// main
