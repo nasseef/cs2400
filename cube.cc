7b3cfeae46579de96216a39b3964de8da86b07a4
Nasseef Abukamail <abukamai@ohio.edu>
2023-10-18 12:03:37 -0400

    cube function example

--- cube.cc
+++ cube.cc
@@ -15,15 +15,17 @@ int cube(int value);
 
 int main(int argc, char const *argv[]) {
 
-    int x = 3;
+    int value = 3;
     int y;
-    y = cube(x);
+    y = cube(value);
     cout << "cube is " << y << endl;
+    cout << "value is " << value << endl; //stays at 3
     return 0;
 } /// main
 
 int cube(int value){// value is a formal parameter
     int result;  //local variable
     result = value * value * value;
+    value += 5;  //only changes inside the function cube
     return result;
 }
