fe9f642e84592eeaf5da8c65db04d84cf091f07d
Nasseef Abukamail <abukamai@ohio.edu>
2023-10-18 12:01:33 -0400

    input stream with checking for input failure

---
+++ input-stream-fail.cc
@@ -0,0 +1,40 @@
+/**
+ *   @file: input-file-stream.cc
+ * @author: Nasseef Abukamail
+ *   @date: October 09, 2023
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+//1
+#include <fstream>
+
+using namespace std;
+
+///Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+    //2
+    ifstream inStream;
+
+    //3 open the file (associate with an external file)
+    inStream.open("data.txt");
+    if (inStream.fail())
+    {
+        cout << "Error: file does not exist" << endl;
+        exit(0);
+    }
+    char ch;
+    //4 read just like cin
+
+    while (inStream.get(ch))
+    {
+        cout << ch;
+        
+    }
+    //5 
+    inStream.close();
+    return 0;
+} /// main
