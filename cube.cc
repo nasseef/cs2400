61255466f82e59162430f806ee3ac4a00f919870
Nasseef Abukamail <abukamai@ohio.edu>
2024-02-19 14:57:29 -0500

    cube function example

---
+++ cube.cc
@@ -0,0 +1,32 @@
+/**
+ *   @file: cube.cc
+ * @author: Nasseef Abukamail
+ *   @date: February 19, 2024
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes or function declaration
+int cube(int value);
+
+int main(int argc, char const *argv[]) {
+
+    int number = 5;
+    int numberCubed;
+    numberCubed = cube(number);
+    cout << "cube is " << numberCubed << endl;
+   
+    cout << cube(-3) << endl;
+    return 0;
+} /// main
+
+//function definition
+int cube(int value){// value is a formal parameter
+    int result;  //local variable
+    result = value * value * value;
+    return result;
+}
