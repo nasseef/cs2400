8702a776e6aff5c9afda95ef226d68af82816b10
Nasseef Abukamail <abukamai@ohio.edu>
2024-04-01 14:50:05 -0400

    Vector of structures

--- student-struct.cc
+++ student-struct.cc
@@ -18,6 +18,11 @@ struct Student{
 }; //; is required
 
 ///Constants and function prototypes
+Student getStudentData();
+void displayStudent(Student s);
+void updatScore(Student &s);
+
+void displayAllStudents(const vector<Student> &allStudents);
 
 int main(int argc, char const *argv[]) {
 
@@ -30,12 +35,51 @@ int main(int argc, char const *argv[]) {
 
     Student s2 = {"Sue", 19, 90};
 
+    Student s3 = getStudentData();
+    displayStudent(s3);
+
     vector<Student> allStudents;
     allStudents.push_back(s);
     allStudents.push_back(s2);
+    allStudents.push_back(s3);
+
+    cout << "-------------------------" << endl;
+    displayAllStudents(allStudents);
 
     cout << "Name: " << allStudents.at(0).name << endl;
 
 
     return 0;
 } /// main
+
+void displayStudent(Student s){
+    cout << " Student name: " << s.name << endl;
+    cout << "   Student ID: " << s.id << endl;
+    cout << "Student Score: " << s.score << endl;
+}
+
+Student getStudentData(){
+    Student temp;
+    cout << "Enter student name: ";
+    cin >> temp.name;
+    cout << "Enter student ID: ";
+    cin >> temp.id;
+    cout << "Enter student score: ";
+    cin >> temp.score;
+    return temp;
+}
+
+void updatScore(Student &s){
+    s.score += 10;
+}
+
+void displayAllStudents(const vector<Student> &allStudents){
+    for (size_t i = 0; i < allStudents.size(); i++)
+    {
+        displayStudent(allStudents.at(i));
+    }
+    
+    for(Student s :  allStudents){
+        displayStudent(s);
+    }
+}
