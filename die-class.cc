6e31d20134bdfc38379cb48c049dc7891500ee47
Nasseef Abukamail <abukamai@ohio.edu>
2024-04-24 11:40:57 -0400

    Die class with getters, setters, and roll function

--- die-class.cc
+++ die-class.cc
@@ -5,47 +5,73 @@
  *  @brief: Add Description
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
-class Die{
-public:
+class Die {
+   public:
     Die();
     Die(int newNumSides);
+
+    // setters/mutators
+    void setNumSides(int newNumSides);
+
+    // getters/accessors
+    int getNumSides();
+    int getFaceValue();
+
+    //helper functions
     void output();
-private: 
+    void roll();
+
+   private:
     int numSides;
     int faceValue;
-
 };
-///Constants and function prototypes
+/// Constants and function prototypes
 
 int main(int argc, char const *argv[]) {
     Die d1(12);
     d1.output();
     cout << endl;
-    
-    return 0;
-} /// main
+    d1.roll();
+    d1.output();
+    cout << endl;
 
-Die::Die(){
+    d1.setNumSides(20);
+    d1.output();
+    cout << endl;
+    return 0;
+}  /// main
+
+Die::Die() {
     numSides = 6;
     srand(time(0));
     faceValue = rand() % numSides + 1;
 }
 
-    Die::Die(int newNumSides){
-        numSides = 6;
-        if (newNumSides > 1)
-        {
-            numSides = newNumSides;
-        }
-        
-        srand(time(0));
-        faceValue = rand() % numSides + 1;
+Die::Die(int newNumSides) {
+    numSides = 6;
+    if (newNumSides > 1) {
+        numSides = newNumSides;
     }
-void Die::output(){
-    cout << "Face Value: " << faceValue;
+
+    srand(time(0));
+    faceValue = rand() % numSides + 1;
+}
+void Die::output() { cout << "Face Value: " << faceValue; }
+
+void Die::roll(){
+    faceValue = rand() % numSides + 1;
+}
+
+void Die::setNumSides(int newNumSides){
+    if (newNumSides > 1)
+    {
+        numSides = newNumSides;
+        roll();
+    }
+    
 }
