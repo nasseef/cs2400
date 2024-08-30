373b991b8b9f2c538f9de8c790c3d144f97ab801
Nasseef Abukamail <abukamai@ohio.edu>
2024-08-30 11:55:10 -0400

    first program

---
+++ first.cc
@@ -0,0 +1,20 @@
+/**
+ *   @file: first.cc
+ * @author: Nasseef Abukamail
+ *   @date: August 30, 2024
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main() {
+
+    ///start of the program
+    cout << "Hello CS 2400!!" << endl;
+    return 0;
+} /// 
