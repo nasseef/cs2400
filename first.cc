95985ae602cf13f27ae58a25d2a1c71f52c10c28
Nasseef Abukamail <abukamai@ohio.edu>
2024-09-19 13:39:51 -0400

    first program

--- first.cc
+++ first.cc
@@ -10,11 +10,20 @@
 #include <cstdlib>
 using namespace std;
 
-///Constants and function prototypes
+/// Constants and function prototypes
 
-int main() {
+int main()
+{
 
-    ///start of the program
-    cout << "Hello CS 2400!!" << endl;
+    /// start of the program
+    // declared a variable of type string and initialized it
+    string message = "Welcome to OU";
+
+    cout << "Hello CS2400!!" << endl;
+    cout << message << endl;
+    cout << "length of message is " << message.length() << endl;
+    cout << "The second character is " << message.at(1) << endl;
+    char first = message.at(0);
+    cout << "First character is " << first << endl;
     return 0;
-} /// 
+} ///
