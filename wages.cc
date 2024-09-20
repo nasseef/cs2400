8977f4416179802bde2544b89c4548f64f834bb3
Nasseef Abukamail <abukamai@ohio.edu>
2024-09-20 11:41:03 -0400

    Added a y/n loop

--- wages.cc
+++ wages.cc
@@ -5,47 +5,51 @@
  *  @brief: Add Description
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
-///Constants and function prototypes
+/// Constants and function prototypes
 
 int main(int argc, char const *argv[]) {
-
     double hours, rate, wages;
-    //input
-    cout << "Enter the hours: ";
-    cin >> hours;
-    //validate the hours
-    // if (hours < 0)
-    // {
-    //     cout << "Number of hours must be greater than or equal to 0" << endl;
-    //     exit(0);
-    // }
-    // if (hours > 39)
-    // {
-    //      cout << "Number of hours must be less than or equal to 39" << endl;
-    //     exit(0);
-    // }
-    
-    if (hours < 0 || hours > 39)
-    {
-       cout << "Hours must between 0 and 39" << endl;
-       exit(0);
-    }
-    
-    cout << "Hours: " << hours << endl;
-    cout << fixed << setprecision(2);
+    char answer;
+    // input
+    do {
+        cout << "Enter the hours: ";
+        cin >> hours;
+        // validate the hours
+        //  if (hours < 0)
+        //  {
+        //      cout << "Number of hours must be greater than or equal to 0" <<
+        //      endl; exit(0);
+        //  }
+        //  if (hours > 39)
+        //  {
+        //       cout << "Number of hours must be less than or equal to 39" <<
+        //       endl;
+        //      exit(0);
+        //  }
 
-    cout << "Enter the rate: ";
-    cin >> rate;
-    //validate the rate
-    cout << "Rate: $" << rate << endl;
-    ///3. Calculations
-    wages = hours * rate;
-    cout << "Wages: $" << wages << endl;
+        if (hours < 0 || hours > 39) {
+            cout << "Hours must between 0 and 39" << endl;
+            exit(0);
+        }
+
+        cout << "Hours: " << hours << endl;
+        cout << fixed << setprecision(2);
+
+        cout << "Enter the rate: ";
+        cin >> rate;
+        // validate the rate
+        cout << "Rate: $" << rate << endl;
+        /// 3. Calculations
+        wages = hours * rate;
+        cout << "Wages: $" << wages << endl;
+        cout << "Do you want to continue (y/n)? ";
+        cin >> answer;
+    } while (answer == 'y');
 
     return 0;
-} /// main
+}  /// main
