541e8ec75ec4c3d36483b486e68f072f26b8f6c3
Nasseef Abukamail <abukamai@ohio.edu>
2024-09-20 16:07:34 -0400

    Sentinel loop example with summation and average calculation

--- sentinel-loop.cc
+++ sentinel-loop.cc
@@ -5,26 +5,34 @@
  *  @brief: Example using sentinel loop
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
 /// Constants and function prototypes
 
-int main()
-{
-
+int main() {
     double total = 0;
-    double score;
+    double score, average;
+    int count = 0;
     cout << "Enter a score (negative number to quit): ";
-    cin >> score;  //initialization step
-    while (score >= 0) //condition
+    cin >> score;       // initialization step
+    while (score >= 0)  // condition
     {
+        count++;
         total += score;
         cout << "Enter a score (negative number to quit): ";
-        cin >> score;   //update step
+        cin >> score;  // update step
     }
     cout << "Total is " << total << endl;
+    if (count > 0) {
+        average = total / static_cast<double>(count);
+        cout << "Average is " << average << endl;
+    }
+    else {
+        cout << "Average cannot be calculated" << endl;
+    }
+
     return 0;
-} ///
+}  ///
