d256f774eed37ba59fa1e4752f78ad6526dbbb61
Nasseef Abukamail <abukamai@ohio.edu>
2024-10-09 11:25:25 -0400

    Reference parameter example

---
+++ swap.cc
@@ -0,0 +1,36 @@
+/**
+ *   @file: swap.cc
+ * @author: Nasseef Abukamail
+ *   @date: February 23, 2024
+ *  @brief: Add Description
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream> 
+using namespace std;
+
+/// Constants and function prototypes
+void swap(int &num1, int &num2);
+
+int main(int argc, char const *argv[]) {
+    int first = 100, second = 200;
+    cout << "Before swap" << endl;
+    cout << "first = " << first << endl;
+    cout << "second = " << second << endl;
+
+    swap(first, second);
+    cout << "*******************" << endl;
+    cout << "After swap" << endl;
+    cout << "first = " << first << endl;
+    cout << "second = " << second << endl;
+
+    return 0;
+}  /// main
+
+void swap(int &num1, int &num2) {
+    int temp;
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
