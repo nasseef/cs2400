3699527c71dab667ee31d2df38fd651d610544ff
Nasseef Abukamail <abukamai@ohio.edu>
2025-01-17 14:18:40 -0500

    first C++ program

---
+++ first.cc
@@ -0,0 +1,19 @@
+/**
+ *   @file: first.cc
+ * @author: Nasseef Abukamail
+ *   @date: January 17, 2025
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main() {
+
+    cout << "Hello World!" << endl;
+    return 0;
+} /// main
