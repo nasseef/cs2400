4d12d18993802ebda069d40ae26ecfea4491efab
Nasseef Abukamail <abukamai@ohio.edu>
2025-02-07 17:30:37 -0500

    Input failure example

---
+++ input-failure.cc
@@ -0,0 +1,38 @@
+/**
+ *   @file: sentinel.cc
+ * @author: Nasseef Abukamail
+ *   @date: February 07, 2025
+ *  @brief: Add Description
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+/// Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+    double total;
+    int count = 0;
+    total = 0;
+    double number;
+    cout << "Enter a number, q to quit: ";
+    cin >> number; //I
+    while (!cin.fail()) { //C
+        count++;
+        total += number;
+        cout << "Enter a number, q to quit: ";
+        cin >> number; //U
+    }
+
+    if (cin.fail())
+    {
+        cin.clear(); //clear the flag
+        cin.ignore(100, '\n');
+    }
+    
+    cout << "Total is " << total << endl;
+    cout << "Count is " << count << endl;
+    return 0;
+}  /// main
