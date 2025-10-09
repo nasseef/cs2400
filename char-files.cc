10d6e2f672534001fd14bf7d29119094577a934b
Nasseef Abukamail <abukamai@ohio.edu>
2025-10-08 21:24:47 -0400

    example reading characters and strings from a file including get and getline

---
+++ char-files.cc
@@ -0,0 +1,48 @@
+
+/**
+ *   @file: files.cc
+ * @author: Nasseef Abukamail
+ *   @date: October 08, 2025
+ *  @brief: Add Description
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+/// 1
+#include <fstream>
+
+using namespace std;
+
+/// Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+    /// 2
+    ifstream inStream;
+    /// 3
+    inStream.open("numbers.txt");
+    if (inStream.fail()) {
+        cout << "Error: file does not exist" << endl;
+        exit(0);
+    }
+    /// 4 use it
+    // char ch;
+    // inStream.get(ch);
+    // while (!inStream.eof()) {
+    //     cout << ch;
+    //     inStream.get(ch);
+    // }
+    // cout << endl;
+    string line;
+    getline(inStream, line);
+
+    while (!inStream.eof()) {
+        cout << line << endl;
+        getline(inStream, line);
+    }
+    cout << endl;
+
+    /// 5
+    inStream.close();
+    return 0;
+}  /// main
