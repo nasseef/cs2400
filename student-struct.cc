c068d06057bf1f3b52a8d45bccf299ba544dd702
Nasseef Abukamail <abukamai@ohio.edu>
2025-11-07 15:08:01 -0500

    Vector of structure objects

--- student-struct.cc
+++ student-struct.cc
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <fstream>
+
 using namespace std;
 
 struct Student{
@@ -17,6 +19,7 @@ struct Student{
 };
 ///Constants and function prototypes
 void printStudent(const Student &theStudent);
+void printAllStudents(const vector<Student> &allStudents);
 
 int main(int argc, char const *argv[]) {
     Student s;
@@ -25,10 +28,26 @@ int main(int argc, char const *argv[]) {
     s.score = 55;
     cout << "The first student" << endl;
     printStudent(s);
+    
     Student s2 = {"Sue", 976, 90.0};
     cout << "The second student" << endl;
     printStudent(s2);
 
+    cout << "Printing all Students" << endl;
+    vector <Student> allStudents;
+    ifstream ins;
+    ins.open("students.txt");
+    if (ins.fail())
+    {
+        cout << "File does not exist" << endl;
+        exit(0);
+    }
+    
+    //read student data into the vector allStudents
+    //importStudents(allStudents, ins);
+    ins.close();
+
+    printAllStudents(allStudents);
     return 0;
 } /// main
 
@@ -36,4 +55,14 @@ void printStudent(const Student &theStudent){
     cout << " Name: " << theStudent.name << endl;
     cout << "   ID: " << theStudent.id << endl;
     cout << "Score: " << theStudent.score << endl;
+}
+
+void printAllStudents(const vector<Student> &allStudents){
+    for (size_t i = 0; i < allStudents.size(); i++)
+    {
+        printStudent(allStudents.at(i));
+    }
+    // for(Student s : allStudents){
+    //     printStudent(s);
+    // }
 }
