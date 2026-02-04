636b9f48823adaba8ac388ee8aa153657af7022d
Nasseef Abukamail <abukamai@ohio.edu>
2026-02-04 10:42:02 -0500

    Example calculate area of square

--- area.cc
+++ area.cc
@@ -11,18 +11,31 @@
 using namespace std;
 
 ///Constants and function prototypes
+const double PI = 3.14;
+const double TAX_RATE = 0.07;
 
 int main(int argc, char const *argv[]) {
-
+   
     //variable declarations
-    double side = 5.5;
+    double length;
+    double width;
     double area;
+    // string name;
+    // cout << "Enter your name: ";
+    // cin >> name;
+    //input
+    cout << "Enter the rectangle length: ";
+    cin >> length;
+    cout << "Enter the rectangle width: ";
+    cin >> width;
 
     //calculation
-    area = side * side;
+    area = length * width;
 
     //output the results
-    cout << "The area of the square is " << area << endl;
+    cout << "The length is " << length << endl;
+    cout << "The width is " << width << endl;
+    cout << "The area of the rectangle is " << area << endl;
 
     return 0;
 } /// main
