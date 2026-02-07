9a3e0b704ce32f7bf9f6e00a2614d0b955c13e84
Nasseef Abukamail <abukamai@ohio.edu>
2026-02-07 07:45:46 -0500

    Added a loop

--- wages.cc
+++ wages.cc
@@ -5,41 +5,44 @@
  *  @brief: Add Description
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
-///Constants and function prototypes
+/// Constants and function prototypes
 
-int main(int argc, char const *argv[]) {
-
+int main(int argc, char const* argv[]) {
+    char answer;
     double rate, hours, wages;
-    //input the values
-    cout << "Enter the number of hours: ";
-    cin >> hours;
-    if (hours < 0 || hours > 40) //invalid
-    {
-        cout << "Hours must be between 0 and 40" << endl;
-        exit(0); //exit(EXIT_SUCCESS)
-    }
-    
-    cout << "Enter the hourly rate: ";
-    cin >> rate;
-    if (rate < 10 || rate > 40)
-    {
-        cout << "Rate must be between 10 and 40" << endl;
-        exit(0);
-    }
-    
+    do {
+        // input the values
+        cout << "Enter the number of hours: ";
+        cin >> hours;
+        if (hours < 0 || hours > 40)  // invalid
+        {
+            cout << "Hours must be between 0 and 40" << endl;
+            exit(0);  // exit(EXIT_SUCCESS)
+        }
 
-    //Calculate the wages
-    wages = rate * hours;
+        cout << "Enter the hourly rate: ";
+        cin >> rate;
+        if (rate < 10 || rate > 40) {
+            cout << "Rate must be between 10 and 40" << endl;
+            exit(0);
+        }
 
-    //output the results
-    cout << "Number of hours: " << hours << endl;
-    cout << "Hourly rate: " << rate << endl;
-    cout << fixed << setprecision(2);
-    cout << "Wages: $" << wages << endl;
+        // Calculate the wages
+        wages = rate * hours;
+
+        // output the results
+        cout << "Number of hours: " << hours << endl;
+        cout << "Hourly rate: " << rate << endl;
+        cout << fixed << setprecision(2);
+        cout << "Wages: $" << wages << endl;
+        
+        cout << "Process more employees (y/n)? ";
+        cin >> answer;
+    } while (answer == 'y');
     return 0;
-} /// main
+}  /// main
