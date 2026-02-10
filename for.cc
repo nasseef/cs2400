435471f45f7628f4ce9f848fe5aacc6a3e3107fd
Nasseef Abukamail <abukamai@ohio.edu>
2026-02-09 20:05:07 -0500

    Example for loop

---
+++ for.cc
@@ -0,0 +1,25 @@
+/**
+ *   @file: for.cc
+ * @author: Nasseef Abukamail
+ *   @date: February 09, 2026
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    //size_t is "unsigned int"
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout << "Jump" << endl;
+    }
+    
+
+    return 0;
+} /// main
