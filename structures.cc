75cab971360bc53e34549562b85e8260550280b1
Nasseef Abukamail <abukamai@ohio.edu>
2026-04-03 11:16:24 -0400

    Added vector of Student objects

--- structures.cc
+++ structures.cc
@@ -20,15 +20,24 @@ struct Student{
 ///Constants and function prototypes
 void printStudent(const Student &s);
 void inputStudent(Student &s);
+void printAllStudents(const vector<Student> &allStudents);
 
 int main(int argc, char const *argv[]) {
-    Student s1;
+    Student s1 = {234, "Sue", 66};
     Student s2 = {123, "Jim", 87};
+    Student s3 = {456, "Bob", 55};
     inputStudent(s1);
     printStudent(s1);
 
     printStudent(s2);
     
+    vector<Student> allStudents;
+    allStudents.push_back(s1);
+    allStudents.push_back(s2);
+    allStudents.push_back(s3);
+
+    printAllStudents(allStudents);
+
     return 0;
 } /// main
 
@@ -45,4 +54,13 @@ void inputStudent(Student &s){
     cin >> s.id;
     cout << "Enter the score: ";
     cin >> s.score;
+}
+
+void printAllStudents(const vector<Student> &allStudents){
+
+    for (size_t i = 0; i < allStudents.size(); i++)
+    {
+       printStudent(allStudents.at(i));
+    }
+    
 }
