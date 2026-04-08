8f5e5f9d23edf94f4db78ecba9dff5cf21db3362
Nasseef Abukamail <abukamai@ohio.edu>
2026-04-08 11:44:32 -0400

    Added constructors

--- student-class.cc
+++ student-class.cc
@@ -13,6 +13,12 @@ using namespace std;
 // Student docs
 class Student{
 public:
+    //default constructor (initialize to default values)
+    Student();
+    Student(int newId);
+    Student(int newId, string newName);
+    Student(int newId, string newName, double newScore);
+
     //setters/mutators
     void setId(int newId);
     void setName(string newName);
@@ -22,6 +28,9 @@ public:
     int getId();
     string getName();
     double getScore();
+
+    //helper functions
+    void output();
 
 private:
     int id;
@@ -33,22 +42,38 @@ private:
 void printStudent(const Student &s);
 
 int main(int argc, char const *argv[]) {
-    Student s1;
+    Student s1; //automatically calls the default constructor
     s1.setId(5);
-    cout << s1.getId() << endl;
+    //cout << s1.getId() << endl;
+    s1.setId(-100);
+    s1.output();
     
-  
-    //printStudent(s1);
-    
+    Student s2(100); //call the second constructor
+    s2.output();
     return 0;
 } /// main
 
+Student::Student(){
+    id = 0;
+    name = "N/A";
+    score = -1;
+}
+
+Student::Student(int newId){
+    id = 0;
+    if (newId > 0)
+    {
+        id = newId;
+    }
+    name = "N/A";
+    score = -1;
+    
+}
 int Student::getId(){
     return id;
 }
 void Student::setId(int newId){
-    id = 0;
-    if (newId > 0)
+    if (newId >= 0)
     {
         id = newId;
     }
@@ -56,12 +81,10 @@ void Student::setId(int newId){
 }
 
 
-
-
-// void printStudent(const Student &s){
-//     cout << " Name: " << s.name << endl;
-//     cout << "   ID: " << s.id << endl;
-//     cout << "Score: " << s.score << endl;
-// }
+void Student::output(){
+    cout << " Name: " << name << endl;
+    cout << "   ID: " << id << endl;
+    cout << "Score: " << score << endl;
+}
 
 
