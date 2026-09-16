a06dd4e6c6206d81d2df4c2953adf4cb3ff72555
Nasseef Abukamail <abukamai@ohio.edu>
2026-09-16 15:59:50 -0400

    wages with y/n loop

--- wages.cc
+++ wages.cc
@@ -5,44 +5,45 @@
  *  @brief: Add Description
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
-///Constants and function prototypes
+/// Constants and function prototypes
 
-int main(int argc, char const *argv[]) {
-
-    //variable declaration
-    double hours; 
+int main(int argc, char const* argv[]) {
+    // variable declaration
+    double hours;
     double rate;
     double wages;
-
-    cout << fixed;  //default is scientific
+    char answer;
+    cout << fixed;  // default is scientific
     cout << setprecision(2);
-    //input
-    cout << "Enter the hours: ";
-    cin >> hours;
-    if (hours < 0 || hours > 40)
-    {
-        cout << "Error: hours must be between 0 and 40 hours" << endl;
-        exit(0);
-    }
-    
-    cout << "Hours: " << hours << endl;
-    cout << "Enter the rate: ";
-    cin >> rate;
-    if (rate < 10 || rate > 50)
-    {
-        cout << "Error: rate must be between 10 and 50" << endl;
-        exit(0);
-    }
-    
-    cout << "Rate: $" << rate << " per hour" << endl;
-    //calculations
-    wages = rate * hours;
-    //output
-    cout << "Wages: $" << wages << endl;
+    do {
+        // input
+        cout << "Enter the hours: ";
+        cin >> hours;
+        if (hours < 0 || hours > 40) {
+            cout << "Error: hours must be between 0 and 40 hours" << endl;
+            exit(0);
+        }
+
+        cout << "Hours: " << hours << endl;
+        cout << "Enter the rate: ";
+        cin >> rate;
+        if (rate < 10 || rate > 50) {
+            cout << "Error: rate must be between 10 and 50" << endl;
+            exit(0);
+        }
+
+        cout << "Rate: $" << rate << " per hour" << endl;
+        // calculations
+        wages = rate * hours;
+        // output
+        cout << "Wages: $" << wages << endl;
+        cout << "Continue (y/n)? ";
+        cin >> answer;  //initialized and updated
+    } while (answer == 'y'); //condition
     return 0;
-} /// main
+}  /// main
