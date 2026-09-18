9723ebecf87e35df1c46b23a3b4113913753c494
Nasseef Abukamail <abukamai@ohio.edu>
2026-09-18 13:58:41 -0400

    Example while loop

---
+++ while.cc
@@ -0,0 +1,25 @@
+/**
+ *   @file: while.cc
+ * @author: Nasseef Abukamail
+ *   @date: September 18, 2026
+ *  @brief: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+///Constants and function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    int jumps = 0;  //I
+    while (jumps < 5) //C
+    {
+        cout << "Jump" << endl;
+        jumps++; //U
+    }
+    
+    return 0;
+} /// main
